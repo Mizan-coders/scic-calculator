5c4f67c266a23e836430fbc685e3fd6ebeb10ad9
Mizanur Rahman <52274346+Webmizan@users.noreply.github.com>
2022-09-19 23:48:07 +0600

    scic calculator project

---
+++ js/common.js
@@ -0,0 +1,12 @@
+const assignmentContainer = document.getElementById('assignment-container');
+const totalAssignment = document.getElementById('assignment-amount');
+
+const submittedIn = select =>  select.options[select.selectedIndex].value;
+
+const obtainedInput = (input) => {
+    const select = input.parentNode.parentNode.children[2].children[1];
+    const submission = submittedIn(select);
+
+    const value = Number(input.value);
+    input.value = (value>0 && value<=submission) ? value : 0;
+}
